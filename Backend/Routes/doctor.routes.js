cb4cb2eb4035bc0410df68de0f15004292037db0
Mohamed Ahmed <155320492+MohamedAbdelhamid0@users.noreply.github.com>
2025-12-23 00:23:14 +0200

    Delete Backend directory

--- Backend/Routes/doctor.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const doctorController = require("../Controllers/doctor.controller");
-
-
-router.route('/classrooms').post(doctorController.bookClassroom); // DONE EAV MODEL
-router.get("/courses/:doctorId", doctorController.getDoctorCourses);
-
-module.exports = router;
--- Backend/Routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userAuthController = require("../Controllers/userAuth.controller");
-
-router.post("/signup", userAuthController.signUp); // DONE EAV MODEL
-router.post("/signin", userAuthController.signIn); // DONE EAV MODEL
-
-module.exports = router;
