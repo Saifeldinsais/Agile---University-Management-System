d4890bc64d1679ab996a2945d9aefe38dd4df6d5
Mohamed Ahmed <155320492+MohamedAbdelhamid0@users.noreply.github.com>
2025-12-23 00:24:08 +0200

    Delete frontend directory

--- frontend/src/pages/NotFound.jsx
+++
@@ -1,5 +0,0 @@
-function NotFound() {
-  return <h1>404 – Page Not Found</h1>;
-}
-
-export default NotFound;
--- frontend/src/pages/NotImplemented.jsx
+++
@@ -1,6 +0,0 @@
-function NotImplemented(){
-    return(
-        <h1>To Be Implemented In the Coming Phases</h1>
-    )
-}
-export default NotImplemented;
